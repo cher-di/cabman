34ae2dab1be027c7cf2831655861caac3a7fe4dc
cher-di <cher-di@mail.ru>
2022-05-25 22:55:43 +0500

    Add method to get full image url

--- internal/edemrf/api.go
+++ internal/edemrf/api.go
@@ -6,9 +6,14 @@ import (
 )
 
 const API_SERVER_URL = "https://api.edemrf.com/v22"
+const WEB_SERVER_URL = "https://едем.рф"
 
 func GetEndpoint(endpoint string) *url.URL {
 	parsedUrl, _ := url.Parse(API_SERVER_URL)
 	parsedUrl.Path = path.Join(parsedUrl.Path, endpoint)
 	return parsedUrl
+}
+
+func GetImageFullUrl(relativeUrl string) string {
+	return WEB_SERVER_URL + relativeUrl
 }
